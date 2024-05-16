45c0341a22c44b798b35b195f3ac0ac0b80bd3d0
Muhammad Rahman Prihadi <mrahman.21053@mhs.unesa.ac.id>
2024-05-16 20:46:18 +0700

    fix

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,22 +1,29 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { auth } from "../lib/firebase";
+import { useRouter } from "next/navigation";
 const DashboardPage = () => {
   const user = auth.currentUser;
   console.log({ user });
+  const router = useRouter();
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  });
   return (
     <div>
       <div className="w-full h-96 bg-[url('/DashboardBackground.jpg')]">
         <div className="flex h-full items-center px-48 gap-4 bg-black/50">
           <img
-            src={user.photoURL}
+            src={user?.photoURL}
             width={150}
             alt=""
             className="rounded-full"
           />
           <div className="text-white">
             <h1 className="text-5xl font-semibold drop-shadow-sm">
-              {user.displayName}
+              {user?.displayName}
             </h1>
             <p className="text-3xl text-white/70">Dasbor Siswa</p>
           </div>
